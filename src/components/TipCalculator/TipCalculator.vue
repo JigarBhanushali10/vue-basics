aa25d306b11dfb565095a2e913da207cb69df831
JigarBhanushali10 <jigard.bhanushali12@gmail.com>
2022-12-27 15:04:09 +0530

    feat(feat: assests):  TipCalculator,bootstrap
    added bootstrap to existing project, added new component Tip Calculator.

---
+++ src/components/TipCalculator/TipCalculator.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="calucatorBody w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <div>
+            <figure class="mb-5 pb-5">
+                <img src="../../assets/images/logo.svg" alt="logo" class="img-fluid">
+            </figure>
+        </div>
+        <div class="d-flex bg-white">
+            <div class="p-3">
+                <div>
+
+                    Bill
+                    <div>
+                        
+                        <input type="text" class="border-0 w-100 bg-light outline-none">
+                    </div>
+                </div>
+                <div>
+                    Select Tip %
+                    <div>
+                        <button class="btn btn-dark py-2 px-4">
+5%
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="bg-dark rounded-5 p-3">
+                <div class="py-3 px-1">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+
+  css: {
+
+    loaderOptions: {
+
+      scss: {
+
+        additionalData: `@import "~@/styles.scss";`
+
+      }
+
+    }
+
+  }
+
+}
